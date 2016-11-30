49213dc1e2656778ce9f8c9781a3c30a5307cf8b
JessCodes <jessicafellison@gmail.com>
2016-11-29 16:36:07 -0800

    Ajaxed things

--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,38 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+  registrationButtonListener();
+  loginButtonListener();
+
 });
+
+var registrationButtonListener = function(){
+  $('.header-main').on('click', '.register-link', function(event){
+    event.preventDefault();
+    // console.log(event, "event");
+    // console.log(this, "this");
+
+    var $registerLink = $(this)
+    var url = $registerLink.parent().attr('action')
+    var method = $registerLink.parent().attr('method')
+
+    var request = $.ajax({
+      url: url,
+      method: method
+    })
+    request.done(function(response){
+      $('.header-main').prepend(response);
+      $('#register-form').toggle();
+    })
+  })
+}
+
+var loginButtonListener = function(){
+  $('.header-main').on('click', '.login-link', function(event){
+    event.preventDefault();
+    // console.log(event, "event");
+    // console.log(this, "this");
+    
+  })
+}
